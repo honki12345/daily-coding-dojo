819a2070db2b14ae945f3e6a0a16720e1c97c7e3
honki12345 <iui4793@gmail.com>
2023-08-16 13:09:14 +0900

    9oorm-challenge

---
+++ 9oormthon-challenge/README.md
@@ -0,0 +1,4 @@
+# 구름톤 챌린지
+
+![9oorm1](./image/9oorm1.png)
+![9oorm1](./image/9oorm2.png)
---
+++ 9oormthon-challenge/week01/day01/day01.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		String[] stringArray = input.split(" ");
+		Double w = Double.parseDouble(stringArray[0]);
+		Double r = Double.parseDouble(stringArray[1]);
+		
+		int result = (int)Math.floor(w * (1 + r / 30));
+		System.out.println(result);
+	}
+}
