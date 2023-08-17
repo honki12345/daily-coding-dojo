63a8fd02598504d7cec3ec8a1da791c8e802900f
honki12345 <iui4793@gmail.com>
2023-08-17 13:39:42 +0900

    구름톤: 완벽한_햄버거_만들기.java

---
+++ 9oormthon-challenge/week01/완벽한_햄버거_만들기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int times = scanner.nextInt();
+		long sum = 0;
+		int before = 0;
+		boolean upFlag = true;
+		boolean downFlag= false;
+		boolean perfectFlag = true;
+		
+		for (int i = 0; i < times; i++) {
+			int input = scanner.nextInt();
+			if (upFlag) {
+				if (input >= before) {
+					sum += input;
+					before = input;
+				} else {
+					upFlag = false;
+					sum += input;
+					before = input;
+				}
+			} else {
+				if (input <= before) {
+					sum += input;
+					before = input;
+				} else {
+					perfectFlag = false;
+				}
+			}
+		}
+		
+		if (perfectFlag) {
+			System.out.println(sum);
+		} else {
+			System.out.println(0);
+		}
+	}
+}
